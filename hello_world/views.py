e863ad71c117fbf5bf105a5cc8fd89f5410100fd
Rachel Cutler <rach_cutler@live.co.uk>
2023-11-14 11:23:55 +0000

    fixed hello world view file

--- hello_world/views.py
+++ hello_world/views.py
@@ -3,7 +3,7 @@ from django.http import HttpResponse
 
 # Create your views here.
 def index(request):
-    if request.method == POST:
+    if request.method == "POST":
         return HttpResponse("You must have POSTed something")
-    elif request.method == GET:
+    else:
         return HttpResponse(request.method)
--- my_project/settings.py
+++ my_project/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-4!ng^^q+2g5!(h=^gwtaw^smue55kx)=gf=f_@sbnyd*dl40yr
 DEBUG = False
 
 ALLOWED_HOSTS = ['8000-rachbry-djangoproject-ndjblv2q86n.ws-eu106.gitpod.io',
-                ,'.herokuapp.com']
+                '.herokuapp.com']
 
 
 # Application definition
